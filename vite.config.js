6218c2e35ba1d072bb1d72da144b2dfdbd856866
Arbind <arbindmalava@gmail.com>
2025-04-10 18:40:43 +0530

    Update configuration for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/Sem-4/',
   root: 'src', // Set the root directory where Vite should look for files
   build: {
     outDir: '../dist', // Output directory after building the project
